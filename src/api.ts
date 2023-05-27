6baf641d4dfd3c15568340b5708ee15e41fa40ae
Luis S <luisalejandross@dynameat.io>
2023-05-27 23:13:33 +0200

    fix por the api endpoint

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import { PlanetResponseApi, Planet } from './types/planet';
 
-const API_URL = import.meta.env.VTIE_STAR_WARS_ENDPOINT
-const GET_PLANETS_ENDPOINT = (page = '1') => `${API_URL}/planets/?page=${page}`
+const API_URL = import.meta.env.VITE_STAR_WARS_ENDPOINT
+const GET_PLANETS_ENDPOINT = (page = 1) => `${API_URL}/planets/?page=${page}`
 
 export const getPlanetListRequest = async (): Promise<Planet[]> => {
-    let pageKey = '1';
+    let pageKey = 1;
     let keepFetching = true;
     const planetList: Planet[] = [];
     while (keepFetching) {
         try {
          const response = await axios.get<PlanetResponseApi>(GET_PLANETS_ENDPOINT(pageKey));
          const { data } = response;
-         planetList.concat(data.results);
+         planetList.push(...data.results);
          if (data.next === null) {
              keepFetching = false;
              continue
@@ -20,6 +20,7 @@ export const getPlanetListRequest = async (): Promise<Planet[]> => {
          pageKey += 1;
         } catch (e) {
             console.log(e)
+            keepFetching = false;
         }
     }
     return planetList
