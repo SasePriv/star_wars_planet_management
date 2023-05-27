529948a65e6e5abf67ab1c940086c5813bd12a54
Luis S <luisalejandross@dynameat.io>
2023-05-27 23:13:19 +0200

    some update to app and planet view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { useEffect } from "react";
-import PlanetList from './views/planetList';
-import { StoreContextProvider } from "./store/store-context";
+import {useContext, useEffect} from "react";
+import PlanetView from './views/planetView';
+import {ACTIONS, StoreContext } from "./store/store-context";
+import {getPlanetListRequest} from "./api";
+import {Planet} from "./types/planet";
+import {ContextType} from "./types/state";
 
 function App() {
+    const {  dispatch }: ContextType = useContext(StoreContext);
+
+    useEffect(() => {
+        dispatch({ type: ACTIONS.TOGGLE_LOADING, payload: true })
+        getPlanetListRequest()
+            .then((planetList: Planet[]) => {
+                dispatch({ type: ACTIONS.ADD_PLANET_LIST, payload: planetList })
+                dispatch({ type: ACTIONS.TOGGLE_LOADING, payload: false })
+        });
+    }, [])
+
   return (
-      <StoreContextProvider>
-        <PlanetList/>
-      </StoreContextProvider>
+        <PlanetView/>
   )
 }
 
--- src/views/planetView/index.tsx
+++ src/views/planetView/index.tsx
@@ -1,9 +1,13 @@
 import CardPlanetInfo from '../../components/cardPlanetInfo';
 import StarWarsLogo from '../../assets/images/star-wars-logo.png';
 import './style.css';
-import { useEffect } from "react";
+import {useContext, useEffect} from "react";
+import {ContextType} from "../../types/state";
+import {StoreContext} from "../../store/store-context";
 
 function PlanetList() {
+    const {  state }: ContextType = useContext(StoreContext);
+
     return (
         <div className="container">
             <div className="logo-container">
