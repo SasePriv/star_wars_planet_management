06d8a75862c18da92bb332562285ca926f21b01d
Luis Sanchez <luisalejandross@dynameat.io>
2023-05-30 01:17:20 +0200

    finish the implementaion fo confirmationModal component

---
+++ src/components/confirmationModal/index.tsx
@@ -0,0 +1,36 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import './style.css'
+
+interface ConfirmationModalProps {
+    onHide: () => void;
+    show?: boolean;
+    onConfirm: () => void;
+    title: string
+}
+
+function ConfirmationModal({ show, onHide, onConfirm, title }: ConfirmationModalProps) {
+    return(
+        <Modal
+          show={show}
+          onHide={onHide}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+                {title}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <div className="btn-confirmation">
+                  <Button variant="danger" onClick={onConfirm}>Accept</Button>
+                  <Button variant="primary" onClick={onHide}>Cancel</Button>
+              </div>
+          </Modal.Body>
+        </Modal>
+      );
+}
+
+export default ConfirmationModal;
