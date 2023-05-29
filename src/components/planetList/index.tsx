6d4a2eaa748e445f6f4314bea2c586a722ec7619
Luis Sanchez <luisalejandross@dynameat.io>
2023-05-30 01:18:27 +0200

    finish the implementaion fo planetList component

--- src/components/planetList/index.tsx
+++ src/components/planetList/index.tsx
@@ -4,16 +4,25 @@ import './style.css'
 
 interface Props {
     planetList: Planet[]
+    detailPlanet: (planet: Planet) => void
+    editPlanet: (planet: Planet) => void
+    deletePlanet: (planet: Planet) => void
 }
 
-function PlanetList({ planetList }: Props) {
+function PlanetList({ planetList, detailPlanet, editPlanet, deletePlanet }: Props) {
     return (
         <div className="card-list">
             {planetList.map((planet: Planet, index:number) => (
-                <CardPlanetInfo planet={planet} key={index}/>
+                <CardPlanetInfo
+                    planet={planet}
+                    key={index}
+                    onEdit={editPlanet}
+                    onDelete={deletePlanet}
+                    onDetail={detailPlanet}
+                />
             ))}
         </div>
     )
 }
 
-export default PlanetList
+export default PlanetList;
