ae26462c2fe9035d10d334adfaec728b72037ea1
Luis Sanchez <luisalejandross@dynameat.io>
2023-05-30 01:17:03 +0200

    finish the implementaion fo cardPlanetInfo component

--- src/components/cardPlanetInfo/index.tsx
+++ src/components/cardPlanetInfo/index.tsx
@@ -5,30 +5,48 @@ import Taootine from '../../assets/images/tatooine.jpeg'
 import './style.css'
 
 interface Props {
-    planet: Planet
+    planet: Planet;
+    onDetail: (planet: Planet) => void;
+    onEdit: (planet: Planet) => void;
+    onDelete: (planet: Planet) => void;
 }
 
-function CardPlanetInfo({ planet }: Props) {
+function CardPlanetInfo({ planet, onDetail, onEdit, onDelete }: Props) {
     const cardStyle: React.CSSProperties = {
         backgroundImage: `linear-gradient(180deg, rgba(255, 255, 255, 0.03) 10%, 
         rgba(0, 0, 0, 0.9) 100%), url(${Taootine})`,
       };
-      return (
-        <div className="card" style={cardStyle}>
+
+    const handleDetails = (e) => {
+        e.stopPropagation();
+        onDetail(planet)
+    }
+
+    const handleEdit= (e) => {
+        e.stopPropagation();
+        onEdit(planet)
+    }
+
+    const handleDelete= (e) => {
+        e.stopPropagation();
+        onDelete(planet)
+    }
+
+    return (
+        <div onClick={handleDetails} className="card" style={cardStyle}>
           <div className="options">
-              <div className="options-icons fav-icon"><AiFillHeart/></div>
-              <div className="options-icons"><AiFillEdit/></div>
-              <div className="options-icons"><AiFillDelete/></div>
+              <div onClick={handleEdit} className="options-icons"><AiFillEdit/></div>
+              <div onClick={handleDelete} className="options-icons"><AiFillDelete/></div>
           </div>
           <div className="card-description">
-              <h2 className="planet-name">Taooine</h2>
-              <p className="planet-items">Diameter: 10465</p>
-              <p className="planet-items">Climate: arid</p>
-              <p className="planet-items">Terrain: desert</p>
-              <p className="planet-items">Population: 200000</p>
+              <h2 className="planet-name">{planet.name}</h2>
+              <p className="planet-items">Diameter: {planet.diameter}</p>
+              <p className="planet-items">Climate: {planet.climate}</p>
+              <p className="planet-items">Terrain: {planet.terrain}</p>
+              <p className="planet-items">Population: {planet.population}</p>
           </div>
         </div>
-      );
+    );
 
 }
 
